a26bfbd0191c622c34f9414839e0ba612bf1392e
LIAM HAN <liam.xciv@gmail.com>
2019-02-05 20:33:45 -0800

    fixes

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 
 stuff = {'boob' : [1,3,4,4], 'penis': [5,6,7,7]}
-appearances = []
+appearances = [[]]
 counter = 0
 for key, values in stuff.items():
     count = 0
@@ -10,13 +10,11 @@ for key, values in stuff.items():
         print(value)
         if value not in appearances[counter]:
             appearances.append(None)
-            appearances[counter] = (value, count)
+            appearances[counter].append([value, count])
+            print(appearances[counter])
         else:
             print('else ')
-            appearances[counter].append(value, count + 1)
+            appearances[counter][-1] = [value, count + 1 ]
 
     counter += 1
 print(appearances)
-
-
-
